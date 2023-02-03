4eff42a098ef248c7d1c62aa58b6c64d3e74820b
Pavel Tyukin <tyukin.pavel@yandex.ru>
2023-02-03 08:28:04 +0300

    feat(shortener): add tests

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,16 +6,14 @@ import (
 	"github.com/paveltyukin/practicum-go-shortener/internal/app/server"
 	"github.com/paveltyukin/practicum-go-shortener/internal/app/shortener"
 	"github.com/paveltyukin/practicum-go-shortener/internal/app/storage"
-	"github.com/paveltyukin/practicum-go-shortener/internal/config"
 	"github.com/paveltyukin/practicum-go-shortener/pkg/logger"
 )
 
 func main() {
 	l := logger.InitLogger()
-	cfg := config.InitConfig(l)
 	s := shortener.New(l)
 	st := storage.New(l)
-	err := server.Serve(cfg, s, st)
+	err := server.Serve(s, st)
 	if err != nil {
 		log.Fatal(err)
 	}
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -5,14 +5,12 @@ package server
 //go:generate mockery --name=handlerShortener --testonly --inpackage
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"time"
 
 	"github.com/paveltyukin/practicum-go-shortener/internal/app/shortener"
 	"github.com/paveltyukin/practicum-go-shortener/internal/app/storage"
-	"github.com/paveltyukin/practicum-go-shortener/internal/config"
 )
 
 var _ Handler = &handler{}
@@ -80,9 +78,9 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func Serve(cfg *config.Config, s shortener.Shortener, st storage.Storage) error {
+func Serve(s shortener.Shortener, st storage.Storage) error {
 	server := &http.Server{
-		Addr:           fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
+		Addr:           "127.0.0.1:8080",
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
